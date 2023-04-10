2b1e2fe5407c240c9df44e5027b89e05653a4db8
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-04-10 07:36:42 +0100

    enable listener for orders service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 import { app } from '../../app'
 import { natsWrapper } from '../../nats-wrapper'
+import { Ticket } from '../../model/ticket'
 
 it('returns a 404 if provided id does not exist', async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -122,4 +123,30 @@ it('publishes an event', async () => {
     .expect(200)
 
     expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
+
+it('rejects updates if the ticket is reserved', async() => {
+    const cookie = global.signin()
+
+    const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+        title: 'making',
+        price: 100,
+    })
+    .expect(201)
+
+    const ticket = await Ticket.findById(response.body.id)
+    ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString()})
+    await ticket!.save()
+
+    await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+        title: 'new',
+        price: 200
+    })
+    .expect(400)
 })
