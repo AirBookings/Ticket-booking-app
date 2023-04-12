ef6195f50440bb181f236010711f0ab589d369fb
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-04-13 00:17:00 +0100

    setup payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,24 @@
+import express, { urlencoded } from 'express'
+import 'express-async-errors'
+import { json } from "body-parser";
+import cookieSession from 'cookie-session';
+import { currentUser, errorHandler, NotFoundError } from '@uc-tickets/common';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(urlencoded({ extended: false}))
+app.use(cookieSession({
+    signed: false, // disabled encryption
+    secure: process.env.NODE_ENV !== 'test', // must be on https connection
+}))
+app.use(currentUser)
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler)
+
+export { app }
