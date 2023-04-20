7813f34479906136dc0ce398aad33b8a77b2e909
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-04-20 18:47:13 +0100

    updated

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-    console.log('starting auth service:::::::')
+    console.log('starting auth service:::::::', process.env.JWT_KEY)
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
